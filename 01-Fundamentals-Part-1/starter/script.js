6f9ab8e62f5f112d5e6558c5a17c23e7c4a062cc
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-10 22:01:09 -0700

    variables and first assignment

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Cameron');
+console.log(23);
+
+let firstName = "Katelyn";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "cameron";
+let PI = 3.14159;
+
+// good variable names
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// bad variable names
+let job1 = 'programmer';
+let job2 = 'teacher';
