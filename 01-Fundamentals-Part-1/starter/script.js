c5095d89803b802d4376c4a46f7d1f7c4cca36b8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-12 01:00:07 -0700

    operators and precedence lecture notes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -44,7 +44,6 @@ console.log(typeof year);
 
 year = 1991;
 console.log(typeof year);
-*/
 
 // Declaring Variables----------------------
 let age = 30;
@@ -54,3 +53,52 @@ const birthYear = 1991;
 
 var birthMonth = 'May'; // old method of declaring variables, use only for legacy code
 birthMonth = 'July';
+
+// Operators--------------------------------
+// Math Operators
+const now = 2021
+const ageCameron = now - 1990;
+const ageJerry = now - 1985;
+console.log(ageCameron, ageJerry);
+
+console.log(ageCameron * 2, ageCameron / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3, which equals 8
+
+const firstName = 'Cameron';
+const lastName = 'Kingsley';
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x /= 5; // x = x / 5 = 20
+x++; // x = x + 1
+console.log(x);
+
+// Comparison Operators
+console.log(ageCameron > ageJerry); // < > <= >=
+console.log(ageJerry >= 18);
+
+const isFullAge = ageJerry >= 18;
+console.log(isFullAge);
+
+console.log(now - 1990 > now - 1985);
+*/
+// Operator Precedence----------------------
+const now = 2021
+const ageCameron = now - 1990;
+const ageJerry = now - 1985;
+console.log(now - 1990 > now - 1985);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageCameron + ageJerry) / 2;
+console.log(ageCameron, ageJerry);
+console.log(averageAge);
