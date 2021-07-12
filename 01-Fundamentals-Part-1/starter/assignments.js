e20491b66458aa89dfac0d0d50fe515980628a1c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-12 00:59:50 -0700

    Comment out assignments

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,11 +1,11 @@
 // Assignment #1 - Variables---------------------
-// /*
+/*
 const country = "USA";
 const continent = "North America";
 let population = 330;
 
 console.log(country + ', ' + continent + ', Population: ' + population + " million");
-// */
+
 
 // Assignment #2 - Data Types--------------------
 const isIsland = false;
@@ -13,3 +13,4 @@ let language;
 
 language = 'English';
 console.log("Is Island?: " + isIsland + ", Language: " + language);
+*/
