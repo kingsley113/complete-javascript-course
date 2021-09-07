c2fa76ddf076ad49935de49be6426f598b01e4d8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-06 17:01:02 -0700

    Breakpoint lessons

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
+    // C) fix bug
+    value: Number(prompt('Degrees celsius:')),
+    // value: 10,
+  };
+  // B) find bug
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify bug
+console.log(measureKelvin());
